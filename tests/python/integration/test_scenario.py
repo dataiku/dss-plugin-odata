6534db548351b0f2323c9003d5cae136396f6bfc
Alexandre Bourret <alexandre.bourret@dataiku.com>
2022-04-15 13:56:58 +0200

    Adding pagination integration tests

---
+++ tests/python/integration/test_scenario.py
@@ -0,0 +1,15 @@
+from dku_plugin_test_utils import dss_scenario
+
+TEST_PROJECT_KEY = "PLUGINTESTSAPODATA"
+
+
+def test_run_sap_odata_v2(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="ODATA_V2")
+
+
+def test_run_sap_odata_v3(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="ODATA_V3")
+
+
+def test_run_sap_odata_v4(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="ODATA_V4")
